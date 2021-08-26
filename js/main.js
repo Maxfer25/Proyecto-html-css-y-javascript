448d97cb234750656324aeda19de9649fc831214
Max <maxifernandezr5@gmail.com>
2021-08-26 18:58:16 -0400

    Desarrollo de la pagina contact us dando diseño en ambas versiones

--- js/main.js
+++ js/main.js
@@ -14,13 +14,7 @@ let enlacesheader = document.querySelectorAll(".enlaces-header")[0];
 let semaforo = true;
 
 document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
-    if(semaforo){
-        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
-        semaforo=false;
-    }else{
-        document.querySelectorAll(".hamburguer")[0].style.color="#000";
-        semaforo=true;
-    }
+   
     
     enlacesheader.classList.toggle("menudos");
 })
